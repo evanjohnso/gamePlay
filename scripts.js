21785b57773e721f0cfa2c8292e9a9c9b3d9dc75
ej <evanrjohnso@gmail.com>
2017-10-01 21:12:30 -0700

    create basic canvas with square and motion

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
-alert('yo yo');
+let canvas = document.getElementById('myCanvas');
+//set width and height to CSS properties
+canvas.width = canvas.scrollWidth;
+canvas.height = canvas.scrollHeight;
+
+console.log("width: ", canvas.width, "height: ", canvas.height);
+
+let ctx = canvas.getContext('2d');
+var square = {
+    corner: [0,0],
+    width: 50,
+    height: 50,
+    color: "red",
+    draw: function() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.corner[0], this.corner[1], this.width, this.height);
+    }
+};
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    square.corner[0] +=2;
+    square.corner[1] +=2;
+    square.draw();
+}
+
+draw();
+var intervalId = setInterval(draw, 50);
